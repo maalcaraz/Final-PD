f4a0449ea04e53ee67efc386e625560041c0d316
maalcaraz <mariangelalcaraz@hotmail.com>
2017-12-09 23:10:32 -0300

    Update aux.hs

--- sudoku/src/aux.hs
+++ sudoku/src/aux.hs
@@ -2,6 +2,7 @@
  -- ['1','-', '-', '-','-','4', '-', '1','2','-', '1', '-','-','-', '-', '-']
 import Data.Char
 import System.IO
+import Data.Maybe
 	
 main :: IO ()
 main = do 
@@ -30,20 +31,37 @@ toColumnas :: [[Int]] -> [[Int]]
 toColumnas [] = []
 toColumnas f = transpose f
 
--- Funcion que separa el tablero en cuadros
-toCuadros :: [[Int]] -> [[Int]]
-toCuadros [] = []
-toCuadros (a:b:as) = toCuadros' a b ++ toCuadros as 
-
+-- Funcion que separa el tablero en cuadrantes
+toCuadrantes :: [[Int]] -> [[Int]]
+toCuadrantes [] = []
+toCuadrantes (a:b:as) = toCuadrantes' a b ++ toCuadrantes as 
 -- Auxiliar de la funcion de arriba
-toCuadros' :: [Int] -> [Int] -> [[Int]]
-toCuadros' [] [] = []
-toCuadros' a b = ((take 2 a)++(take 2 b)) : (toCuadros' (drop 2 a) (drop 2 b)) 
+toCuadrantes' :: [Int] -> [Int] -> [[Int]]
+toCuadrantes' [] [] = []
+toCuadrantes' a b = ((take 2 a)++(take 2 b)) : (toCuadrantes' (drop 2 a) (drop 2 b)) 
 
+
+-- crea una tupla con indice de fila|columna|cuadrante y una 
+-- lista con los valores de esa fila|columna|cuadrante
+toTuplas :: [[Int]] -> [(Int , [Int])]
+toTuplas lista = zip [1..] lista 
 
 indices :: [[Int]] -> [(Int, [Int])]
 indices [] = []
 indices (l:ls) = [ (n ,(n `elemIndices` l)) | n <- [1..4] ] ++ indices ls 
+
+
+-- Toma de parametros la fila vieja, el indice donde colocar
+-- el nuevo valor, y el valor en si. Reemplaza el valor unicamente si
+-- es distinto de cero.
+modificarValor :: [Int] -> Int -> Int -> [Int]
+modificarValor lista indice valor 
+			|head (drop indice lista) == 0 = (take (indice) lista) ++ valor : (drop (indice+1) lista)
+			|otherwise = lista
+
+-- Devuelve el indice de la lista que tiene valor 0.
+getIndexUnico :: [Int] -> Int 
+getIndexUnico lista = fromMaybe 1 (findIndex (==0) lista)
 
 -- (snd (indicesFilas !!3!!0)) `intersect` (snd (indicesCols !!0!!0))
 
@@ -62,6 +80,4 @@ indices (l:ls) = [ (n ,(n `elemIndices` l)) | n <- [1..4] ] ++ indices ls
 
 -- Agregar el metodo de insercion de un valor una vez que descubri
 -- la posicion en donde va.
-
--- Agregar la verificacion por cuadrante.
 
