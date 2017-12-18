d5ed0185d154817ceebc44632595d001080502c0
maalcaraz <mariangelalcaraz@hotmail.com>
2017-12-18 12:28:24 -0300

    Update Main.hs

--- sudoku/app/Main.hs
+++ sudoku/app/Main.hs
@@ -9,52 +9,63 @@ import System.IO.Error
 import ReadSudokuFile
 import Data.Maybe
 import Sys
+import Sudoku4x4V2
+import System.IO
+import Control.Exception
 
-main :: IO ()
-main = do
-   putStrLn "===============BIENVENIDO A SUDOKU MASTER================"
 
-   putStrLn "Nombre archivo:"
-   nombreFile <- getLine
-   putStrLn "Tamaño:"
-   size <- getLine
-   let sizeInt = readMaybe size :: Maybe Int
-   if sizeInt == Nothing 
-    then do putStrLn "FALLO"
-   else do leerArchivo nombreFile (fromMaybe 0 sizeInt)
 
-   putStrLn "Ingrese el tamaño del tablero (0 para salir): "
-   size <- getLine
+main = toTry `catch` handler
 
-   let op = readMaybe size :: Maybe Int
-   case op of
-    Nothing -> do 
-        putStrLn "No ingreso una opcion valida. Por favor vuelva a ingresar."
-        main
-    op -> do
-        if op == (Just 0) 
-            then do
-                putStrLn "CHAU"
-                return()
-            else if op == (Just 4)
-                then do 
-                    putStrLn "\n====================================================== \n"
-                    putStrLn "Resolucion tablero 4x4 (Ejemplo 4): \n"
-                    let sudokuResolution4x4ej4 = resolverSudoku4x4 $ tableroSudoku4x4 4
-                    printSolucionTableros4x4 sudokuResolution4x4ej4
-                    main
-                else if op == (Just 9)
-                    then do
-                        putStrLn "Ingresaste 9" 
-                        main
-                    else if op == (Just 16)
-                        then do
-                            putStrLn "Ingresaste 16"
-                            main
-                        else do 
-                            putStrLn "Ingresaste un tamaño invalido, vuelve a intentarlo!"
-                            main
+toTry :: IO ()
+toTry = do    
+      putStrLn "===============BIENVENIDO A SUDOKU MASTER================"
 
+		putStrLn "Nombre del archivo:"
+      nombreFile <- getLine
+		putStrLn "Ingrese el tamaño: "
+      size <- getLine
+      let sizeInt = readMaybe size :: Maybe Int
+      if sizeInt == Nothing 
+       then do putStrLn "FALLO"
+      else do leerArchivo nombreFile (fromMaybe 0 sizeInt)
+
+      putStrLn "Ingrese el tamaño del tablero (0 para salir): "
+      size <- getLine
+
+      let op = readMaybe size :: Maybe Int
+      case op of
+       Nothing -> do 
+           putStrLn "No ingreso una opcion valida. Por favor vuelva a ingresar."
+           main
+       op -> do
+           if op == (Just 0) 
+               then do
+                   putStrLn "CHAU"
+                   return()
+               else if op == (Just 4)
+                   then do 
+                       putStrLn "\n====================================================== \n"
+                       putStrLn "Resolucion tablero 4x4 (Ejemplo 4): \n"
+                       let sudokuResolution4x4ej4 = resolverSudoku4x4 $ tableroSudoku4x4 4
+                       printSolucionTableros4x4 sudokuResolution4x4ej4
+                       main
+                   else if op == (Just 9)
+                       then do
+                           putStrLn "Ingresaste 9" 
+                           main
+                       else if op == (Just 16)
+                           then do
+                               putStrLn "Ingresaste 16"
+                               main
+                           else do 
+                               putStrLn "Ingresaste un tamaño invalido, vuelve a intentarlo!"
+                               main
+
+handler :: IOError -> IO ()
+handler e
+    | isDoesNotExistError e = putStrLn "The file doesn't exist!"
+    | otherwise = ioError e
 
 --    putStrLn "\n====================================================== \n"
 --    putStrLn "Resolucion tablero 9x9: \n"
